1a43e4877c78366314f4be983a83fa63ffa183fa
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2021-05-23 23:21:02 -0400

    Time: 12 ms (99.75%), Space: 13.8 MB (53.67%) - LeetHub

--- number-of-provinces/number-of-provinces.cpp
+++ number-of-provinces/number-of-provinces.cpp
@@ -8,7 +8,7 @@ public:
         root.resize(n);
         for(int i=0;i<n;i++) root[i]=i;
         
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
              for(int j=i+1;j<n;j++){
                  if(isConnected[i][j]==1){
                      int root1=getRoot(i);
