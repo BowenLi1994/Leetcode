cce453d334fd129aafa049fc4041c849dc29c337
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2021-12-23 23:00:57 -0500

    Time: 40 ms (26.30%), Space: 71.5 MB (47.29%) - LeetHub

--- add-two-numbers/add-two-numbers.cpp
+++ add-two-numbers/add-two-numbers.cpp
@@ -11,90 +11,40 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-        ListNode* dummy=new ListNode(-1);
-        ListNode* curr=dummy;
+        ListNode* sum=new ListNode();
         int carry=0;
+        ListNode* result=sum;
+        
         while(l1 || l2){
-            int val1=l1?l1->val:0;
-            int val2=l2?l2->val:0;
-            int sum=val1+val2+carry;
-            carry=sum/10;
-            ListNode* temp=new ListNode(sum%10);
-            curr->next=temp;
-            curr=curr->next;
-            if(l1) l1=l1->next;
-            if(l2) l2=l2->next;
-        }
-        if(carry!=0){
-            ListNode* temp=new ListNode(carry);
-            curr->next=temp;
+            int temp=0;
+            if(l1){
+                temp+=l1->val;
+                l1=l1->next;
+            } 
+            if(l2){
+                temp+=l2->val;
+                l2=l2->next;
+            } 
+            
+            temp+=carry;
+            carry=temp/10;
+            temp%=10;
+            
+            ListNode* node= new ListNode(temp);
+            sum->next=node;
+            sum=node;
+            
+            
+            
         }
         
-        return dummy->next;
+        if(carry!=0){
+            ListNode* node= new ListNode(carry);
+            sum->next=node;
+            sum=node;
+        }
+        
+        return result->next;
         
     }
 };
-
-
-/*Shiiiiit!!!!*/
-// class Solution {
-// public:
-//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-//         ListNode*  head=l1;
-//         int carry=0;
-//         while(l1 && l2){
-//             int temp=l1->val+l2->val+carry;
-//             carry=temp/10;
-//             l1->val=temp%10;
-//             l1=l1->next;
-//             l2=l2->next;
-//         }
-        
-//         if(l1){
-//         while(l1->next){
-//             int temp=l1->val+carry;
-//             carry=temp/10;
-//             l1->val=temp%10;
-//             cout<<"temp: "<<temp<<" carry: "<<carry<<" value: "<<l1->val<<endl; 
-//             l1=l1->next;
-//         }
-//            int temp=l1->val+carry;
-//            carry=temp/10;
-//            l1->val=temp%10;
-//         }
-        
-       
-        
-        
-//         if(l2){
-//             l1=l2;
-//             while(l2->next){
-//                 int temp=l2->val+carry;
-//                 carry=temp/10;
-//                 l2->val=temp%10;
-//                 l2=l2->next;
-//             }
-//            int temp=l2->val+carry;
-//            carry=temp/10;
-//            l2->val=temp%10;
-            
-            
-           
-            
-//         }
-        
-        
-        
-        
-//         if(carry!=0){
-//             ListNode* tail=new ListNode(1);
-//             l1->next=tail;
-//         }
-        
-        
-//         return head;
-        
-//     }
-// };
