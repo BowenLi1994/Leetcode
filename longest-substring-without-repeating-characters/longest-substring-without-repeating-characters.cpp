6ff8d1c45f8fe464de7fc0185ee5b439d217d4d4
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2022-01-02 02:18:04 -0500

    Time: 24 ms (42.01%), Space: 8.6 MB (42.10%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,58 +2,30 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         
-        int res=0;
-        int window_left=-1;
-        unordered_map<char,int> m;
+        if(s.empty())
+            return 0;
         
-        for(int i=0; i< s.size();i++){
-            if(m.count(s[i])&&m[s[i]]>window_left){
-                window_left=m[s[i]];
-            }
+        
+        map<char,int> m;
+        int left=-1;
+        int result=0;
+        
+        for(int i=0;i<s.size();i++){
+            
+            if(m.count(s[i]) && m[s[i]]>left)
+                left=m[s[i]];
             m[s[i]]=i;
-            res=max(res,i-window_left);
+            
+            result=max(result,i-left);
+            
+            // cout<<"--------------"<<endl;
+            // for(auto  kv: m)
+            //     cout<<kv.first<<"--->"<<kv.second<<endl;
+            // cout<<"left: "<<left<<endl;
+            // cout<<"result: "<<result<<endl;
         }
         
-                    
-        return res;
+        return result;
+        
     }
 };
-
-
-//n^2
-// class Solution {
-// public:
-//     int lengthOfLongestSubstring(string s) {
-        
-//         int res=0;
-        
-//         if(s.empty())
-//             return 0;
-//         if(s.size()==1)
-//             return 1; 
-    
-       
-        
-//        for(size_t i=0;i<s.size()-1;i++){
-//            int max_sub=1;
-//            vector<bool> checked(256,false);
-//            checked[s[i]]=true;
-//            for(size_t j=i+1;j<s.size();j++){
-//                if(!checked[s[j]]){
-//                    max_sub++;
-//                    checked[s[j]]=true;
-//                }
-//                else
-//                    break;
-//            }
-           
-//            res=max(res,max_sub);
-           
-//        }
-        
-        
-        
-//         return res;
-        
-//     }
-// };
