9bd6ab25202191ed6d4fc7ca44d29e0cebc2e681
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2022-01-07 00:32:50 -0500

    Time: 21 ms (44.85%), Space: 8.3 MB (69.93%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -10,12 +10,15 @@ public:
         int left=-1;
         
         for(int i=0;i<s.size();i++){
-            if(m.count(s[i]) && left < m[s[i]])
+            
+            if(m.count(s[i]) && m[s[i]] > left){
                 left=m[s[i]];
+            }
+            
             m[s[i]]=i;
+            
             res=max(res,i-left);
         }
-        
         
         
         return res;
