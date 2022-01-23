8c1e6d0a8dbe977822516d79c0df409700b92ff6
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2022-01-23 00:40:39 -0500

    Time: 217 ms (5.08%), Space: 20.1 MB (11.15%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+  
+    struct sort_iv{       
+        bool operator()(const vector<int>& v1, const vector<int>& v2){     
+            return v1[0]<v2[0];
+        }
+    };
+ 
+    
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(),intervals.end(),sort_iv());
+        res.push_back(intervals[0]);
+        
+        int n=intervals.size();
+        
+        
+        
+        for(auto v : intervals)
+            cout<<v[0]<<" "<<v[1]<<endl;
+        
+        for(int i=1;i<n;i++){
+            
+            //cout<<res.back()[0]<<" "<<res.back()[1]<<endl;
+            
+            if(intervals[i][0]>res.back()[1])
+                res.push_back(intervals[i]);
+            else if(intervals[i][1]> res.back()[1]){
+                res.back()[1]=intervals[i][1];
+                //cout<<"**"<<endl;
+            }
+            
+        }
+        
+        return res;
+    }
+};
