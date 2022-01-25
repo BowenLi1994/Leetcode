4579cd773a49ffa6376ea41cf293dbfae6547816
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2022-01-25 00:32:03 -0500

    Time: 0 ms (100.00%), Space: 8.4 MB (70.39%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -13,21 +13,24 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         
+        vector<int> res;
         
-        inorder(root);
+        stack<TreeNode*> st;
+        
+        while(root||!st.empty()){
+            while(root){
+                st.push(root);
+                root=root->left;
+            }
+            
+            root=st.top();
+            st.pop();
+            res.push_back(root->val);
+            root=root->right;
+        }
+        
+        
         return res;
         
     }
-    
-    void inorder(TreeNode* root){
-        if(root==NULL) return;
-        inorder(root->left);
-        res.push_back(root->val);
-        //cout<<root->val<<endl;
-        inorder(root->right);
-    }
-    
-private:
-    vector<int> res;
 };
-
