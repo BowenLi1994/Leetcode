8ef4e59137ecfb8b02639d12227bd815f5aa10d6
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2022-01-27 23:36:49 -0500

    Time: 89 ms (5.52%), Space: 27.6 MB (12.23%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -5,13 +5,10 @@ public:
         int left=0, right=nums.size()-1;
         
         while(left<=right){
-            int mid=(left+right)/2;
-            if(nums[mid]==target)
-                return mid;
-            if(nums[mid] < target)
-                left=mid+1;
-            if(nums[mid] > target)
-                right=mid-1;
+            int mid=(right-left)/2+left;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) right=mid-1;
+            else if(nums[mid]<target) left=mid+1;
         }
         
         return -1;
