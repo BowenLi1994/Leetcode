e82d2179593904fb6c57dbddf4c6adad9a4e60f0
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2022-01-28 00:24:59 -0500

    Time: 11 ms (43.60%), Space: 13.5 MB (98.75%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -24,7 +24,7 @@ public:
         res[0]=left;
         
         
-        left=0, right=nums.size()-1;
+        right=nums.size()-1;
         while(left<=right){
             int mid=(right-left)/2+left;
             if(nums[mid]<target)
