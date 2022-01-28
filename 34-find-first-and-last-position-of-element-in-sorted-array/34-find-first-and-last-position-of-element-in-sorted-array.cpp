589870eb28503e9a9f77b53dfc631093b4fc3722
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2022-01-28 00:24:43 -0500

    Time: 22 ms (5.55%), Space: 13.5 MB (98.75%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -8,33 +8,33 @@ public:
         if(nums.empty())
             return res;
         //left-range search
-        int left=0, right=nums.size();
+        int left=0, right=nums.size()-1;
         
-        while(left<right){
+        while(left<=right){
             int mid=(right-left)/2+left;
             if(nums[mid]<target)
                 left=mid+1;
             else if(nums[mid]>target)
-                right=mid;
+                right=mid-1;
             else if (nums[mid]==target)
-                right=mid;
+                right=mid-1;
         }
         
         if(left==nums.size() || nums[left]!= target) return res;
         res[0]=left;
         
         
-        left=0, right=nums.size();
-        while(left<right){
+        left=0, right=nums.size()-1;
+        while(left<=right){
             int mid=(right-left)/2+left;
             if(nums[mid]<target)
                 left=mid+1;
             else if(nums[mid]>target)
-                right=mid;
+                right=mid-1;
             else if (nums[mid]==target)
                 left=mid+1;
         }
-        res[1]=left-1;
+        res[1]=right;
         
         return res;
         
