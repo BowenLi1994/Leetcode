66d7e2b124514018fce3d5686c8b7d73f4b53eda
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2022-01-30 23:45:12 -0500

    Time: 48 ms (35.60%), Space: 19 MB (34.81%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
-  
-    struct sort_iv{       
-        bool operator()(const vector<int>& v1, const vector<int>& v2){     
-            return v1[0]<v2[0];
-        }
+    struct comp{
+       bool operator()(const vector<int>& v1, const vector<int>&v2){
+           return v1[0]<v2[0];
+       } 
     };
- 
     
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(),intervals.end(),comp());
         vector<vector<int>> res;
-        sort(intervals.begin(),intervals.end(),sort_iv());
+        
         res.push_back(intervals[0]);
         
-        int n=intervals.size();
-        
-        
-        
-//         for(auto v : intervals)
-//             cout<<v[0]<<" "<<v[1]<<endl;
-        
-        for(int i=1;i<n;i++){
-            
-            //cout<<res.back()[0]<<" "<<res.back()[1]<<endl;
-            
+        for(int i=1;i<intervals.size();i++){            
             if(intervals[i][0]>res.back()[1])
                 res.push_back(intervals[i]);
-            else if(intervals[i][1]> res.back()[1]){
-                res.back()[1]=intervals[i][1];
-                //cout<<"**"<<endl;
-            }
             
+            if(intervals[i][1]>res.back()[1])
+                res.back()[1]=intervals[i][1];
         }
         
         return res;
+        
     }
 };
