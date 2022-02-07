cce6c19681387262ebbcb619f574e3f0f88c04e3
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2022-02-06 23:14:21 -0500

    Time: 194 ms (27.44%), Space: 13.4 MB (90.70%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    struct cmp{
+      bool operator()(const string& s1, const string& s2){
+          return s1.size()<s2.size();
+      }  
+    };
+    
+    int longestStrChain(vector<string>& words) {
+        
+        int res=1;
+        sort(words.begin(),words.end(),cmp());
+        
+        int n=words.size();
+        vector<int> dp(n,1);
+        
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(words[j].size()==words[i].size()) continue;
+                if(words[j].size()+1<words[i].size()) break;
+                
+                if(pre(words[i],words[j])){
+                    dp[i]=dp[j]+1;
+                    res=max(res,dp[i]);
+                }
+                
+            }
+        }
+        
+        
+        return res;
+    }
+    
+    bool pre(string l,string s){
+        int i=0,m=l.size(),n=s.size();
+        for(int j=0;j<m;j++)
+            if(l[j]==s[i]) i++;
+        
+        return i==n;
+    }
+};
