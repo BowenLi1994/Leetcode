f6be8549b27d65e5215749d4d4c19212b6b3d630
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2022-02-06 23:21:26 -0500

    Time: 126 ms (48.81%), Space: 17.3 MB (60.10%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -12,19 +12,32 @@ public:
         int res=1;
         sort(words.begin(),words.end(),cmp());
         
-        int n=words.size();
-        vector<int> dp(n,1);
+        // int n=words.size();
+        // vector<int> dp(n,1);
         
-        for(int i=1;i<n;i++){
-            for(int j=i-1;j>=0;j--){
-                if(words[j].size()==words[i].size()) continue;
-                if(words[j].size()+1<words[i].size()) break;
+//         for(int i=1;i<n;i++){
+//             for(int j=i-1;j>=0;j--){
+//                 if(words[j].size()==words[i].size()) continue;
+//                 if(words[j].size()+1<words[i].size()) break;
                 
-                if(pre(words[i],words[j])){
-                    dp[i]=max(dp[i],dp[j]+1);
-                    res=max(res,dp[i]);
+//                 if(pre(words[i],words[j])){
+//                     dp[i]=max(dp[i],dp[j]+1);
+//                     res=max(res,dp[i]);
+//                 }
+                
+//             }
+//         }
+        
+        unordered_map<string,int> dp;
+        for(auto w: words){
+            dp[w]=1;
+            int n=w.size();
+            for(int i=0;i<n;i++){
+                string pre=w.substr(0,i)+w.substr(i+1);
+                if(dp.count(pre)){
+                    dp[w]=max(dp[w],dp[pre]+1);
+                    res=max(res,dp[w]);
                 }
-                
             }
         }
         
@@ -32,11 +45,11 @@ public:
         return res;
     }
     
-    bool pre(string l,string s){
-        int i=0,m=l.size(),n=s.size();
-        for(int j=0;j<m;j++)
-            if(l[j]==s[i]) i++;
+//     bool pre(string l,string s){
+//         int i=0,m=l.size(),n=s.size();
+//         for(int j=0;j<m;j++)
+//             if(l[j]==s[i]) i++;
         
-        return i==n;
-    }
+//         return i==n;
+//     }
 };
