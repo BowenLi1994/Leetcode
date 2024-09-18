4d0192f4f974038687ec21b091e1d7f5a681ad3b
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2024-09-17 23:56:40 -0400

    Time: 43 ms (63.24%), Space: 51 MB (80.71%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-
-        int goal=nums.size()-1;
-
-        for(int i=nums.size()-2;i>=0;i--){
-            if(i+nums[i]>=goal){
-                goal=i;
-            }
+        int goal = nums.size()-1;
+        for (int i=nums.size()-2; i>=0; i--){
+            if(nums[i]+i >= goal)
+                goal = i;
         }
-
-        return !goal;
-        
+        return goal==0;
     }
 };
