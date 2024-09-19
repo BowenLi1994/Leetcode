cbff3d646e0d5057da70fd29cb46a3ce3e2a2924
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2024-09-18 23:35:05 -0400

    Time: 199 ms (11.38%), Space: 103 MB (13.11%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,34 +1,32 @@
 class RandomizedSet {
 private:
     set<int> random_val;
+
 public:
-    RandomizedSet() {
-        random_val.clear();
-    }
-    
+    RandomizedSet() { random_val.clear(); }
+
     bool insert(int val) {
-        if(random_val.count(val)){
+        if (random_val.count(val)) {
             return false;
-        }
-        else{
+        } else {
             random_val.insert(val);
             return true;
-        }      
+        }
     }
-    
+
     bool remove(int val) {
-        if(random_val.count(val)){
+        if (random_val.count(val)) {
             random_val.erase(val);
             return true;
-        }
-        else{
+        } else {
             return false;
-        }   
-        
+        }
     }
-    
+
     int getRandom() {
- return *next(random_val.begin(),rand()%random_val.size());
+        auto it = random_val.begin();
+        advance(it, rand() % random_val.size());
+        return *it;
         // if(random_val.size()==1){
         //     return *random_val.begin();
         // }
