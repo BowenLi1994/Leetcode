aa763491b519c56156151fa4871c35397c7c081d
Bowen Li <33384921+BowenLi1994@users.noreply.github.com>
2024-09-19 00:09:54 -0400

    Time: 3 ms (48.34%), Space: 11 MB (89.02%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-
         sort(citations.begin(),citations.end(),greater());
-        int h_index=1;
-
-        for(auto c:citations){
-            // cout<<c<<endl;
-            if(c>=h_index){
-                h_index++;
-            }
-            else{
-                return h_index-1;
-            }
+        int hIndex = 0;
+        for(int i=0;i<citations.size();i++){
+            if(citations[i]<i+1)
+                break;
+            hIndex = i+1;
         }
-        
-        return h_index-1;
+        return hIndex;
     }
 };
